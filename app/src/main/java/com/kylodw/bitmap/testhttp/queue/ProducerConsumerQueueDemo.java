e913ce1a0b68d92d826e5dea0bb4bfd97c4a1688
3222748221@qq.com <3222748221@qq.com>
2019-04-26 18:30:14 +0800

    使用阻塞队列实现生产者消费者

---
+++ app/src/main/java/com/kylodw/bitmap/testhttp/queue/ProducerConsumerQueueDemo.java
@@ -0,0 +1,28 @@
+package com.kylodw.bitmap.testhttp.queue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author kylodw
+ * @Description:
+ * @Date 2019/04/26
+ * 使用阻塞队列实现生产者消费者
+ */
+public class ProducerConsumerQueueDemo {
+    public static void main(String[] args) {
+
+    }
+
+    static class SharedData {
+        private volatile boolean flag = true;
+        private AtomicInteger atomicInteger = new AtomicInteger();
+
+        BlockingQueue<String> blockingQueue = null;
+
+        public SharedData(BlockingQueue<String> blockingQueue) {
+            this.blockingQueue = blockingQueue;
+        }
+
+    }
+}

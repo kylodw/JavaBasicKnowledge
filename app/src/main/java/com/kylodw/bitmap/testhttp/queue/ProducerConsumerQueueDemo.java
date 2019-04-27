0531b07b382c6b6494cd62c5a5058a70f44d12d0
kylodw <3222748221@qq.com>
2019-04-27 11:54:11 +0800

    生产者消费者阻塞队列实现

--- app/src/main/java/com/kylodw/bitmap/testhttp/queue/ProducerConsumerQueueDemo.java
+++ app/src/main/java/com/kylodw/bitmap/testhttp/queue/ProducerConsumerQueueDemo.java
@@ -1,6 +1,8 @@
 package com.kylodw.bitmap.testhttp.queue;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,6 +13,30 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ProducerConsumerQueueDemo {
     public static void main(String[] args) {
+        SharedData sharedData=new SharedData(new ArrayBlockingQueue<>(10));
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                sharedData.myProd();
+            }
+        },"thread_prod1").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                sharedData.myProd();
+            }
+        },"thread_prod2").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                sharedData.myConsumer();
+            }
+        },"thread_consumer").start();
+
+
 
     }
 
@@ -19,9 +45,55 @@ public class ProducerConsumerQueueDemo {
         private AtomicInteger atomicInteger = new AtomicInteger();
 
         BlockingQueue<String> blockingQueue = null;
-
+        //传接口
         public SharedData(BlockingQueue<String> blockingQueue) {
             this.blockingQueue = blockingQueue;
+            System.out.println(blockingQueue.getClass().getName());
+        }
+        public void stop(){
+            this.flag=false;
+        }
+        public void myProd(){
+            String data=null;
+            boolean returnValue=false;
+            while (flag){
+                data=atomicInteger.incrementAndGet()+"";//++i
+                try {
+                    returnValue=blockingQueue.offer(data,2L, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(returnValue){
+                    System.out.println(Thread.currentThread().getName()+"\t 插入成功");
+                }else{
+                    System.out.println(Thread.currentThread().getName()+"\t 插入失败");
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("生产结束");
+        }
+
+
+
+        public void myConsumer(){
+            String data=null;
+            while (flag){
+                try {
+                    data=blockingQueue.poll(2L,TimeUnit.SECONDS);
+                    if( null==data &&data.equalsIgnoreCase("")){
+                        flag=false;
+                        System.out.println("消费结束");
+                        return;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+"\t 消费队列成功");
+            }
         }
 
     }
